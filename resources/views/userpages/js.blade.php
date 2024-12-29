229343ffe963e541a26367fc2ff0681cb3ff1917
Zeus-M <ahmadmohib127@gmail.com>
2024-12-29 18:19:29 +0500

    Test commit

--- resources/views/userpages/js.blade.php
+++ resources/views/userpages/js.blade.php
@@ -12,3 +12,4 @@
     <script src="{{asset('essence/js/classy-nav.min.js')}}"></script>
     <!-- Active js -->
     <script src="{{asset('essence/js/active.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
